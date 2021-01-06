c2293fcc7baf5c84730e66e7425a56122b1815e3
eox-tecnologia <eox-tecnologia>
2021-01-06 13:31:55 -0300

    Criando e estilizando o componente button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,14 @@ import './Button.css'
 
 export default props => {
     return (
-        <button className="button">{props.label}</button>
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={`button
+            ${props.operation ? 'operation' : ''}
+            ${props.double ? 'double' : ''}
+            ${props.triple ? 'triple' : ''}
+        `}>
+            {props.label}
+        </button>
     )
 }
